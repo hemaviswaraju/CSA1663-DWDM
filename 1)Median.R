8c75f68eeb32cdb84e0a89f3ecfb76dd98148a23
hemaviswaraju <122436783+hemaviswaraju@users.noreply.github.com>
2024-07-23 22:28:25 +0530

    Add files via upload

---
+++ 1)Median.R
@@ -0,0 +1,20 @@
+class_intervals <- data.frame(
+  lower_bound = c(1, 5, 15, 20, 50, 80),
+  upper_bound = c(5, 15, 20, 50, 80, 110),
+  frequency = c(200, 450, 300, 1500, 700, 44)
+)
+
+class_intervals$cumulative_frequency <- cumsum(class_intervals$frequency)
+
+N <- sum(class_intervals$frequency)
+
+median_class_index <- which(class_intervals$cumulative_frequency >= N/2)[1]
+
+L <- class_intervals$lower_bound[median_class_index]
+CF <- if (median_class_index == 1) 0 else class_intervals$cumulative_frequency[median_class_index - 1]
+f <- class_intervals$frequency[median_class_index]
+h <- class_intervals$upper_bound[median_class_index] - class_intervals$lower_bound[median_class_index]
+
+median <- L + ((N/2 - CF) / f) * h
+
+median
